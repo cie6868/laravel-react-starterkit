d2dfa0a821a9284c4a272761255972b7d53ed708
Chamath Ellawala <chamath@dxdy.tech>
2021-05-30 16:18:20 +0530

    FE - Form generator plugin.

---
+++ json-form-react/JsonForm.js
@@ -0,0 +1,96 @@
+import PropTypes from 'prop-types';
+import React, { useCallback, useEffect, useState } from 'react';
+import JsonFormValidator from './utilities/JsonFormValidator';
+
+const JsonForm = (props) => {
+
+  const [formData, setFormData] = useState({});
+  const [formErrors, setFormErrors] = useState({});
+
+  useEffect(() => {
+    const newState = {};
+    props.json.fields.forEach((field) => {
+      newState[field.name] = field.value ? field.value : '';
+    });
+
+    setFormData({
+      ...formData,
+      ...newState,
+    });
+  }, [props.json]);
+
+  const formClasses = `jsfr-form ${props.json.fieldClass}`;
+  const submitLabel = props.json.submit ? props.json.submit.label : 'Submit';
+  const submitClasses = props.json.submit ? props.json.submit.class : '';
+
+  const onChange = useCallback((ev) => {
+    setFormData({
+      ...formData,
+      [ev.target.name]: ev.target.value,
+    });
+  }, [formData]);
+
+  const onSubmitClick = useCallback((ev) => {
+    ev.preventDefault();
+
+    const validator = new JsonFormValidator(props.json);
+    const errorList = validator.validateForm(formData);
+    setFormErrors(errorList);
+
+    if (errorList.length === 0) {
+      props.onSubmit(formData);
+    }
+  }, [props.json, formData]);
+
+  const errors = formErrors && Object.entries(formErrors).length > 0 && Object.entries(formErrors).map(([fieldName, errorText]) => {
+    return (
+      <li key={fieldName}>{errorText}</li>
+    );
+  });
+
+  const fields = props.json.fields && props.json.fields.map((field) => {
+    const fieldId = `jsfr-field-${field.name}`;
+    const fieldClasses = `jsfr-field ${field.class} ${formErrors[field.name] ? 'jsfr-field-error' : ''}`;
+    const fieldWrapperClasses = `jsfr-field-wrapper ${field.wrapperClass} ${formErrors[field.name] ? 'jsfr-field-wrapper-error' : ''}`;
+
+    return (
+      <div key={field.name} className={fieldWrapperClasses}>
+        <label htmlFor={fieldId}>
+          {field.label}
+        </label>
+
+        <input
+          id={fieldId}
+          type={field.type}
+          name={field.name}
+          value={formData[field.name] || ''}
+          placeholder={field.placeholder}
+          className={fieldClasses}
+          onChange={onChange}/>
+      </div>
+    );
+  });
+
+  return (
+    <form className={formClasses}>
+      {errors && <ul className="jsfr-errors">{errors}</ul>}
+
+      {fields}
+
+      <button
+        type="submit"
+        className={submitClasses}
+        onClick={onSubmitClick}>
+        {submitLabel}
+      </button>
+    </form>
+  );
+
+};
+
+JsonForm.propTypes = {
+  json: PropTypes.object,
+  onSubmit: PropTypes.func,
+};
+
+export default React.memo(JsonForm);
---
+++ json-form-react/utilities/JsonFormValidator.js
@@ -0,0 +1,107 @@
+export default class JsonFormValidator {
+
+  constructor(json) {
+    this.json = json;
+  }
+
+  validateForm(values) {
+    const errorList = {};
+    for (const [fieldName, fieldValue] of Object.entries(values)) {
+      const errorText = this._validateField(fieldName, fieldValue);
+      if (errorText) {
+        errorList[fieldName] = errorText;
+      }
+    }
+    return errorList;
+  }
+
+  _validateField(fieldName, fieldValue) {
+    let returnErrorText = null;
+
+    const fieldDef = this.json.fields.find((f) => f.name === fieldName);
+    for (const rule of fieldDef.validation.split('|')) {
+      const errorText = this._decipherAndValidate(rule, fieldValue);
+      if (errorText) {
+        returnErrorText = errorText.replace('__FIELD_NAME__', this._formatFieldName(fieldName));
+
+        // halt at the first validation error for this field
+        break;
+      }
+    }
+
+    return returnErrorText;
+  }
+
+  /**
+   * Deciphers the validation rule and applies the appropriate validation to return an error text if the value invalid.
+   * @param {String} rule Validation rule with conditions
+   * @param {String} value Field value to be validated
+   * @returns {String|Null} Validation error text for this field, or null
+   */
+  _decipherAndValidate(rule, value) {
+    const ruleTitle = rule.split(':')[0];
+    const ruleConditions = rule.split(':')[1];
+
+    switch (ruleTitle) {
+    case 'required':
+      return this._required(value);
+    case 'min':
+      return this._min(ruleConditions, value);
+    case 'max':
+      return this._max(ruleConditions, value);
+    default:
+      // ignore unknown rules
+      return null;
+    }
+  }
+
+  /**
+   * Make the field name human-readable.
+   * @param {String} fieldName Machine name for the field
+   * @returns {String} Human-readable name for the field
+   */
+  _formatFieldName(fieldName) {
+    return fieldName.toLowerCase().split(/_/g).map(w => w.charAt(0).toUpperCase() + w.substring(1)).join(' ');
+  }
+
+  _required(value) {
+    if (!value) {
+      return 'The value for __FIELD_NAME__ cannot be empty.';
+    }
+
+    return null;
+  }
+
+  _min(condition, value) {
+    const limit = parseInt(condition);
+
+    if (isNaN(value)) {
+      if (value.length < limit) {
+        return `The value for __FIELD_NAME__ should be at least ${limit} characters.`;
+      }
+    } else {
+      if (value < limit) {
+        return `The value for __FIELD_NAME__ should be more than or equal to ${limit}.`;
+      }
+    }
+
+    return null;
+  }
+
+  _max(condition, value) {
+    const limit = parseInt(condition);
+
+    if (isNaN(value)) {
+      if (value.length < limit) {
+        return `The value for __FIELD_NAME__ should be at most ${limit} characters.`;
+      }
+    } else {
+      if (value < limit) {
+        return `The value for __FIELD_NAME__ should be less than or equal to ${limit}.`;
+      }
+    }
+
+    return null;
+  }
+
+}
